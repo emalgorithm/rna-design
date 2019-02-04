f2c3a579a616ffcd9e96ea996d2febbe5dc04646
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2019-02-04 23:53:48 +0000

    Implement code to run scraper

---
+++ src/run_scraper.py
@@ -0,0 +1,7 @@
+from rfam_scraping import RfamScraper
+
+n_families = 3016
+family_ids = ['RF{}'.format(str(i).zfill(5)) for i in range(1, n_families + 1)]
+
+rfam_scraper = RfamScraper()
+sequences = rfam_scraper.store_families_rna_sequences(family_ids)
