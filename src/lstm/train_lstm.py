e0db622fd95b3dc351a6be560e0bdd63cfe58d38
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2019-02-18 15:33:18 +0000

    Train lstm with padded sequences, but without masking in the loss

--- src/lstm/train_lstm.py
+++ src/lstm/train_lstm.py
@@ -1,4 +1,4 @@
-from data_util.data_processing import prepare_sequence
+from data_util.data_processing import prepare_sequence, my_collate
 from lstm.lstm_model import LSTMModel
 import torch
 import torch.nn as nn
@@ -6,6 +6,7 @@ import torch.optim as optim
 from sklearn.metrics import hamming_loss
 from data_util.rna_dataset import RNADataset
 from torchvision import transforms
+from visualization_util import plot_loss
 
 # 'U' and 'T' in this sequences refer both to the base 'U'. 'T' is just used for convenience
 word_to_ix = {"<PAD>": 0, "A": 1, "G": 2, "C": 3, "U": 4, 'T': 4}
@@ -25,11 +26,14 @@ optimizer = optim.SGD(model.parameters(), lr=0.01)
 x_transform = transforms.Lambda(lambda sequences: prepare_sequence(sequences, word_to_ix))
 y_transform = transforms.Lambda(lambda sequences: prepare_sequence(sequences, tag_to_ix))
 
-train_set = RNADataset('../../data/temp_train/', x_transform=x_transform,
-                       y_transform=y_transform)
-test_set = RNADataset('../../data/temp_test/', x_transform=x_transform, y_transform=y_transform)
-train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
-test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False)
+# train_set = RNADataset('../../data/temp_train/', x_transform=x_transform, y_transform=y_transform)
+# test_set = RNADataset('../../data/temp_test/', x_transform=x_transform, y_transform=y_transform)
+train_set = RNADataset('../../data/less_than_40/train/')
+test_set = RNADataset('../../data/less_than_40/test/')
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True,
+                                           collate_fn=my_collate)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False,
+                                          collate_fn=my_collate)
 
 
 def train_epoch(model, train_loader):
@@ -39,7 +43,7 @@ def train_epoch(model, train_loader):
         # Skip last batch if it does not have full size
         if sequences.shape[0] < batch_size:
             continue
-
+        print("Batch {}".format(batch_idx))
         model.zero_grad()
 
         base_scores = model(sequences)
@@ -62,11 +66,19 @@ def train_epoch(model, train_loader):
 
 
 def run(model, n_epochs, train_loader, test_loader):
+    train_losses = []
+    test_losses = []
+
     for epoch in range(n_epochs):
         print("Epoch {}: ".format(epoch + 1))
 
         loss, h_loss = train_epoch(model, train_loader)
         test_loss, test_h_loss = evaluate(model, test_loader)
+
+        train_losses.append(loss)
+        test_losses.append(test_loss)
+
+        plot_loss(train_losses, test_losses)
 
 
 def evaluate(model, test_loader):
@@ -93,4 +105,4 @@ def evaluate(model, test_loader):
         return loss, h_loss
 
 
-run(model, 10, train_loader, test_loader)
+run(model, 50, train_loader, test_loader)
