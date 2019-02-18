4f9a0e1e16ed937e4de2690e3e4dd1abaaa5483f
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2019-02-18 15:37:15 +0000

    Move constants to separate file

---
+++ src/data_util/data_constants.py
@@ -0,0 +1,4 @@
+# 'U' and 'T' in this sequences refer both to the base 'U'. 'T' is just used for convenience
+word_to_ix = {"<PAD>": 0, "A": 1, "G": 2, "C": 3, "U": 4, 'T': 4}
+tag_to_ix = {"<PAD>": 0, ".": 1, "(": 2, ")": 3}
+ix_to_tag = {0: "<PAD>", 1: ".", 2: "(", 3: ")"}
