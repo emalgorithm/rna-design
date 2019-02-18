1e0c6e7080e332e1d95b992a52107056767ff77f
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2019-02-18 17:34:41 +0000

    Add device detection

--- src/data_util/data_constants.py
+++ src/data_util/data_constants.py
@@ -1,4 +1,9 @@
+import torch
+
 # 'U' and 'T' in this sequences refer both to the base 'U'. 'T' is just used for convenience
 word_to_ix = {"<PAD>": 0, "A": 1, "G": 2, "C": 3, "U": 4, 'T': 4}
 tag_to_ix = {"<PAD>": 0, ".": 1, "(": 2, ")": 3}
 ix_to_tag = {0: "<PAD>", 1: ".", 2: "(", 3: ")"}
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+print("The device being used is: {}".format(device))
