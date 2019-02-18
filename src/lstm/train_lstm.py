44f8f1b1f27309d89a9455e85fab6361cbf4007e
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2019-02-18 23:43:54 +0000

    Add validation and model checkpoint

--- src/lstm/train_lstm.py
+++ src/lstm/train_lstm.py
@@ -26,13 +26,17 @@ y_transform = transforms.Lambda(lambda sequences: prepare_sequence(sequences, ta
 # test_set = RNADataset('../../data/temp_test/', x_transform=x_transform, y_transform=y_transform)
 train_set = RNADataset('../data/less_than_40/train/')
 test_set = RNADataset('../data/less_than_40/test/')
+val_set = RNADataset('../data/less_than_40/val/')
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True,
                                            collate_fn=my_collate)
 test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False,
                                           collate_fn=my_collate)
+val_loader = torch.utils.data.DataLoader(val_set, batch_size=batch_size, shuffle=False,
+                                          collate_fn=my_collate)
 
 
 def train_epoch(model, train_loader):
+    model.train()
     avg_loss = 0
     h_loss = 0
     for batch_idx, (sequences, dot_brackets, sequences_lengths) in enumerate(train_loader):
@@ -63,25 +67,36 @@ def train_epoch(model, train_loader):
 def run(model, n_epochs, train_loader, test_loader):
     train_losses = []
     test_losses = []
+    val_losses = []
     train_h_losses = []
     test_h_losses = []
+    val_h_losses = []
 
     for epoch in range(n_epochs):
         print("Epoch {}: ".format(epoch + 1))
 
         loss, h_loss = train_epoch(model, train_loader)
-        test_loss, test_h_loss = evaluate(model, test_loader)
+        test_loss, test_h_loss = evaluate(model, test_loader, mode='test')
+        val_loss, val_h_loss = evaluate(model, val_loader, mode='val')
+
+        if val_h_loss < min(val_h_losses):
+            torch.save(model.state_dict(), '../lstm.pt')
+            print("Saved updated model")
 
         train_losses.append(loss)
         test_losses.append(test_loss)
+        val_losses.append(val_loss)
         train_h_losses.append(h_loss)
         test_h_losses.append(test_h_loss)
+        val_h_losses.append(val_h_loss)
 
-        plot_loss(train_losses, test_losses, file_name='loss2.jpg')
-        plot_loss(train_h_losses, test_h_losses, file_name='h_loss2.jpg', y_label='hamming_loss')
+        plot_loss(train_losses, val_losses, test_losses, file_name='loss2.jpg')
+        plot_loss(train_h_losses, val_h_losses, test_h_losses, file_name='h_loss2.jpg',
+                  y_label='hamming_loss')
 
 
-def evaluate(model, test_loader):
+def evaluate(model, test_loader, mode='test'):
+    model.eval()
     with torch.no_grad():
         loss = 0
         h_loss = 0
@@ -100,8 +115,8 @@ def evaluate(model, test_loader):
         loss /= len(test_loader)
         h_loss /= len(test_loader)
 
-        print("test loss: {}".format(loss))
-        print("test hamming loss: {}".format(h_loss))
+        print("{} loss: {}".format(mode, loss))
+        print("{} hamming loss: {}".format(mode, h_loss))
 
         return loss, h_loss
 
