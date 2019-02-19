46c420754723ee930c723d0f82fb1f8579dc511f
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2019-02-19 16:30:23 +0000

    Refactor and move evaluation function

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,5 +1,6 @@
 from sklearn.metrics import hamming_loss
 import numpy as np
+import torch
 
 
 def masked_hamming_loss(target, pred, ignore_idx=0):
@@ -15,3 +16,33 @@ def compute_accuracy(target, pred, ignore_idx=0):
 
     return accuracy / len(pred)
 
+
+def evaluate(model, test_loader, loss_function, batch_size, mode='test'):
+    model.eval()
+    with torch.no_grad():
+        loss = 0
+        h_loss = 0
+        accuracy = 0
+
+        for batch_idx, (sequences, dot_brackets, sequences_lengths) in enumerate(test_loader):
+            # Skip last batch if it does not have full size
+            if sequences.shape[0] < batch_size:
+                continue
+
+            base_scores = model(sequences, sequences_lengths)
+
+            loss += loss_function(base_scores, dot_brackets.view(-1))
+            pred = base_scores.max(1)[1]
+            h_loss += masked_hamming_loss(dot_brackets.view(-1).cpu().numpy(), pred.cpu().numpy())
+            accuracy += compute_accuracy(dot_brackets.cpu().numpy(), pred.view_as(
+                dot_brackets).cpu().numpy())
+
+        loss /= len(test_loader)
+        h_loss /= len(test_loader)
+        accuracy /= len(test_loader)
+
+        print("{} loss: {}".format(mode, loss))
+        print("{} hamming loss: {}".format(mode, h_loss))
+        print("{} accuracy: {}".format(mode, accuracy))
+
+        return loss, h_loss, accuracy
