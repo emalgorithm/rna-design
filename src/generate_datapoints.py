275cf37e087d4f788bdd0be04c281428006ae968
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2019-02-19 22:19:55 +0000

    Generate train val and test data for sequences less than 450 long

--- src/generate_datapoints.py
+++ src/generate_datapoints.py
@@ -24,55 +24,58 @@ import numpy as np
 #             pickle.dump(obj, open(individual_sequences_path + sequence_file, 'wb'))
 
 
-# def store_sequences(sequences, path):
-#     i = 1
-#     for sequence in sequences:
-#         sequence_file = '{}.rna'.format(i)
-#         i += 1
-#         dot_bracket = RNA.fold(sequence)[0]
-#         adj_matrix = nx.to_scipy_sparse_matrix(dotbracket_to_graph(dot_bracket))
-#         print("Compute structure for sequence {}".format(i))
-#         obj = {
-#             'sequence': sequence,
-#             'dot_bracket': dot_bracket,
-#             # 'family': family,
-#             # 'adj_matrix': adj_matrix,
-#         }
-#         pickle.dump(obj, open(path + sequence_file, 'wb'))
+def store_sequences(sequences, path):
+    i = 1
+    for sequence in sequences:
+        sequence_file = '{}.rna'.format(i)
+        i += 1
+        dot_bracket = RNA.fold(sequence)[0]
+        # adj_matrix = nx.to_scipy_sparse_matrix(dotbracket_to_graph(dot_bracket))
+        print("Compute structure for sequence {}".format(i))
+        obj = {
+            'sequence': sequence,
+            'dot_bracket': dot_bracket,
+            # 'family': family,
+            # 'adj_matrix': adj_matrix,
+        }
+        pickle.dump(obj, open(path + sequence_file, 'wb'))
 
+
+# sequences_with_folding = []
+#
+# family_to_sequences = get_family_to_sequences()
+# i = 0
+#
+# for family, sequences in list(family_to_sequences.items()):
+#     for sequence in sequences:
+#         if len(sequence) <= 450:
+#             dot_bracket = RNA.fold(sequence)[0]
+#
+#             if i % 1000 == 0:
+#                 print("Compute structure for sequence {}".format(i))
+#             i += 1
+#
+#             datapoint = {
+#                 'sequence': sequence,
+#                 'dot_bracket': dot_bracket,
+#                 'family': family,
+#             }
+#             sequences_with_folding.append(datapoint)
+#
+# np.random.shuffle(sequences_with_folding)
+# pickle.dump(sequences_with_folding, open('../data/sequences_with_folding.pkl', 'wb'))
 
 # sequences = np.array([sequence for sequence in get_all_sequences() if len(sequence) <= 100])
 # sequences = get_all_sequences()
-# np.random.shuffle(sequences)
-sequences_with_folding = []
+sequences = pickle.load(open('../data/sequences_with_folding.pkl', 'rb'))
+np.random.shuffle(sequences)
 
-family_to_sequences = get_family_to_sequences()
-i = 0
 
-for family, sequences in list(family_to_sequences.items()):
-    for sequence in sequences:
-        if len(sequence) <= 450:
-            dot_bracket = RNA.fold(sequence)[0]
+train, val, test = np.split(sequences, [int(.8*len(sequences)), int(.9*len(sequences))])
 
-            if i % 1000 == 0:
-                print("Compute structure for sequence {}".format(i))
-            i += 1
-
-            datapoint = {
-                'sequence': sequence,
-                'dot_bracket': dot_bracket,
-                'family': family,
-            }
-            sequences_with_folding.append(datapoint)
-
-np.random.shuffle(sequences_with_folding)
-pickle.dump(sequences_with_folding, open('../data/sequences_with_folding.pkl', 'wb'))
-
-# train, val, test = np.split(sequences, [int(.8*len(sequences)), int(.9*len(sequences))])
-#
-# store_sequences(train, '../data/less_than_100/train/')
-# store_sequences(val, '../data/less_than_100/val/')
-# store_sequences(test, '../data/less_than_100/test/')
+store_sequences(train, '../data/less_than_450/train/')
+store_sequences(val, '../data/less_than_450/val/')
+store_sequences(test, '../data/less_than_450/test/')
 
 
 
