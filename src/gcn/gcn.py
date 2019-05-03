5e85baf7073dc9930b83ccbd8e48e0c528d598a5
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2019-05-02 18:22:47 -0700

    Use geometric package

--- src/gcn/gcn.py
+++ src/gcn/gcn.py
@@ -1,18 +1,19 @@
 import torch.nn as nn
 import torch.nn.functional as F
-# from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GCNConv
 from src.gcn.graph_convolution import GraphConvolution
 
 
 class GCN(nn.Module):
     def __init__(self, n_features, hidden_dim, n_classes, dropout, device='cpu'):
         super(GCN, self).__init__()
-        # self.conv1 = GCNConv(n_features, hidden_dim)
-        # self.conv2 = GCNConv(hidden_dim, n_classes)
-
-        self.conv1 = GraphConvolution(n_features, hidden_dim)
-        self.conv2 = GraphConvolution(hidden_dim, hidden_dim)
-        self.conv3 = GraphConvolution(hidden_dim, n_classes)
+        self.conv1 = GCNConv(n_features, hidden_dim)
+        self.conv2 = GCNConv(hidden_dim, hidden_dim)
+        self.conv3 = GCNConv(hidden_dim, n_classes)
+        #
+        # self.conv1 = GraphConvolution(n_features, hidden_dim)
+        # self.conv2 = GraphConvolution(hidden_dim, hidden_dim)
+        # self.conv3 = GraphConvolution(hidden_dim, n_classes)
 
         self.dropout = dropout
 
