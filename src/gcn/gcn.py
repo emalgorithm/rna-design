3d6afe940a08fb558c191465902555645a29da57
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2019-05-02 17:54:07 -0700

    Change naming

--- src/gcn/gcn.py
+++ src/gcn/gcn.py
@@ -1,20 +1,32 @@
 import torch.nn as nn
 import torch.nn.functional as F
+# from torch_geometric.nn import GCNConv
 from src.gcn.graph_convolution import GraphConvolution
 
 
 class GCN(nn.Module):
-    def __init__(self, n_features, hidden_dim, n_classes, dropout):
+    def __init__(self, n_features, hidden_dim, n_classes, dropout, device='cpu'):
         super(GCN, self).__init__()
+        # self.conv1 = GCNConv(n_features, hidden_dim)
+        # self.conv2 = GCNConv(hidden_dim, n_classes)
 
-        self.gc1 = GraphConvolution(n_features, hidden_dim)
-        self.gc2 = GraphConvolution(hidden_dim, n_classes)
+        self.conv1 = GraphConvolution(n_features, hidden_dim)
+        self.conv2 = GraphConvolution(hidden_dim, hidden_dim)
+        self.conv3 = GraphConvolution(hidden_dim, n_classes)
+
         self.dropout = dropout
 
-    def forward(self, x, adj):
-        x = F.relu(self.gc1(x, adj))
+    def forward(self, data):
+        x, adj, edge_attr = data.x, data.edge_index, data.edge_attr
+
+        x = self.conv1(x, adj)
+        x = F.relu(x)
         x = F.dropout(x, self.dropout, training=self.training)
-        x = self.gc2(x, adj)
-        # return F.log_softmax(x, dim=1)
-        # TODO: Check softmax dimension
-        return F.softmax(x, dim=2)
+
+        x = self.conv2(x, adj)
+        x = F.relu(x)
+        x = F.dropout(x, self.dropout, training=self.training)
+
+        x = self.conv3(x, adj)
+
+        return F.log_softmax(x, dim=1)
