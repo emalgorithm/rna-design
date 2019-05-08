8d80d1f3fd14ecce5d929752b81e2b2a0e507b67
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2019-05-08 01:50:48 -0700

    Fix bug in computing accuracy

--- src/evaluation.py
+++ src/evaluation.py
@@ -254,6 +254,6 @@ def evaluate_family_classifier(model, test_loader, loss_function=None, batch_siz
 
 def compute_metrics_family(target, pred):
     # accuracy = accuracy_score(target, pred)
-    accuracy = (target.eq(pred.long())).sum() / target.shape[0]
+    accuracy = (target.eq(pred.long())).sum().item() / target.shape[0]
 
-    return accuracy.item()
+    return accuracy
